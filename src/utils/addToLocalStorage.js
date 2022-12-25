3abfac42ba2573df472d40446fc8fe292606d151
thekolesnikov <thesharkov@gmail.com>
2022-12-25 13:21:20 +0200

    add build version

--- src/utils/addToLocalStorage.js
+++
@@ -1,7 +0,0 @@
-export function addToLocalStorage(arr) {
-    localStorage.setItem('items', JSON.stringify(arr));
-}
-
-export function getFromLocalStorage() {
-    return JSON.parse(localStorage.getItem('items'));
-}
